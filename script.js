30d68d2220250afc892136eef895b9abe5c7b0f6
AnderG <agibarguen@gmail.com>
2022-08-25 16:25:03 +0200

    Handle click event. Toggle marker and display it on each cell

--- script.js
+++ script.js
@@ -5,8 +5,37 @@ const gameBoard = (function(){
 const displayController = (function(){
     //listen to every click event on every cell of the grid conatiner
     const containerEl = document.querySelectorAll('.item'); //this is a node list (similar to array)
-    const handleClick = ()=>{
+    const X_Marker = 'x';
+    const O_Marker = 'circle';
+    let circleTurn = false;
+     
+    const handleClick = (e)=>{
         console.log('cell clicked')
+        const cell = e.target;
+        let currentClass = circleTurn ? O_Marker : X_Marker;
+        
+        //add a class
+        const addAClass = (cell, currentClass)=>{
+            cell.classList.add(currentClass)
+        };
+        addAClass(cell,currentClass);
+
+        //On every click switch class
+        const switchClass = ()=>{
+            circleTurn = !circleTurn
+        };
+        switchClass();
+
+        //Display Content on Each Cell
+        const displayCellContent = ()=>{
+            cell.textContent = circleTurn ? "X" : "O"
+        };
+        displayCellContent();
+       
+        //Check for Win
+
+
+        //Check for Draw
     }
     containerEl.forEach(cell => cell.addEventListener('click', handleClick, { once:true }))
 })();
