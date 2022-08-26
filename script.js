a4022f58fd59cc978b40d9eebdd8e51d878295a9
AnderG <agibarguen@gmail.com>
2022-08-26 08:44:51 +0200

    Adding the game rules all but the draw rules

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-const gameBoard = (function(){
-    let gameboard = [];
-    return {
-        gameboard,
-    }
+const Gameboard = (function (){
+    let gameboard = []
 })();
 
-const displayController = (function(){
-    //listen to every click event on every cell of the grid conatiner
-    const cellEl = document.querySelectorAll('.item'); //this is a node list (similar to array)
-    const X_Marker = 'x';
-    const O_Marker = 'circle';
-    let circleTurn = false;
-
-    let WINNING_COMBINATIONS = [
+const DisplayController = (function(){
+    const cellEl = document.querySelectorAll('.item')
+    const X_Marker = "x"
+    const O_Marker = "circle"
+    let circleTurn = false
+    const WINNING_COMBINATIONS = [
         [0,1,2],
         [3,4,5],
         [6,7,8],
@@ -22,53 +17,54 @@ const displayController = (function(){
         [0,4,8],
         [2,4,6],
     ];
-     
+
     const handleClick = (e)=>{
-        console.log('cell clicked')
-        const cell = e.target;
-        let currentClass = circleTurn ? O_Marker : X_Marker;
-        //add a class
-        const addAClass = (cell, currentClass)=>{
+        const cell = e.target
+        const currentClass = circleTurn ? O_Marker : X_Marker
+
+        //Add a class when clicking
+        const _addAClass = ()=>{
             cell.classList.add(currentClass)
-        };
-        addAClass(cell,currentClass);
+        }
+        _addAClass()
 
-        //On every click switch class
-        const switchClass = ()=>{
+        //Switch the Turn
+        const _switchTurn = ()=>{
             circleTurn = !circleTurn
-        };
-        switchClass();
+        }
+        _switchTurn()
 
-        //Display Content on Each Cell
-        const displayCellContent = ()=>{
-            cell.textContent = circleTurn ? "X" : "O"
-        };
-        displayCellContent();
-       
-        //Check for Win
-        const checkWinner = ()=>{
-           return WINNING_COMBINATIONS.some(combination =>{
-               return combination.every(index=>{
+        //Display the content 
+        const _displayContent = ()=>{
+          cell.textContent =  circleTurn ? 'X' : 'O'
+        }
+        _displayContent()
+
+        //Check for win
+        const _winner= () =>{
+            return WINNING_COMBINATIONS.some(combination =>{
+                return combination.every(index => {
                     return cellEl[index].classList.contains(currentClass)
                 })
             })
         }
-        if(checkWinner()){console.log('winner')}
-
-
-
-        //Check for Draw
-    }
-    cellEl.forEach(cell => cell.addEventListener('click', handleClick, { once:true }))
+        _winner()
+        
+        //Display winner
+        const _displayWInner = ()=>{
+            if(_winner()){
+                console.log('winner')
+            }
+        }
+        _displayWInner()
+    };
+    cellEl.forEach(cell=> cell.addEventListener('click', handleClick, { once:true }))
 })();
 
 const Player = (playerName)=>{
-    const getName = () => console.log(playerName);
-    const playerTurn = () => console.log(`It's ${playerName}s' turn`)
-    return{playerTurn}
+    const sayName = ()=> console.log(`The name of the player is ${playerName}`)
+    return {sayName}
 };
-
-
 
 
 
