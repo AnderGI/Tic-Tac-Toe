38b85f66c4b7dd6aa420d8232e5a3818b562783d
AnderG <agibarguen@gmail.com>
2022-08-27 18:14:59 +0200

    X or O chooser styling implemented. Logic needs to be implemented

--- introPage/introPage.js
+++ introPage/introPage.js
@@ -1,11 +1,14 @@
 const startGame = (function(){
     const startBtn = document.getElementById('startBtn')
+    const weaponContainer = document.getElementById('chooseWeapon')
+
     const _displayTicTacToe = ()=>{
         const introSection = document.getElementById('introPage')
         introSection.style.cssText=`
         opacity:0;
         z-index:-1;
         `
+
     }
     startBtn.addEventListener('click', _displayTicTacToe)
 })();
