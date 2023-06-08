db315672de94c7054e82e0781495688e703d531a
AnderGI <agibarguen@gmail.com>
2023-06-08 19:25:13 +0200

    HumanPlayer factory added

--- app.js
+++ app.js
@@ -16,3 +16,32 @@ const Gameboard = (function () {
 
   return { getGameboard, resetGameboard };
 })();
+
+const HumanPlayer = (playerMark) => {
+  const mark = playerMark;
+
+  let points = 0;
+
+  const makeMove = (row, col) => {
+    const gameBoard = Gameboard.getGameboard();
+    gameBoard[row][col] = playerMark;
+  };
+
+  const getMarker = () => mark;
+
+  const increasePoints = () => {
+    points = ++points;
+  };
+
+  const getPoints = () => points;
+
+  const setPoints = (_points) => (points = _points);
+
+  return {
+    makeMove,
+    getMarker,
+    increasePoints,
+    getPoints,
+    setPoints,
+  };
+};
